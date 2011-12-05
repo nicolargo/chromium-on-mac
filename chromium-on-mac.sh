763b79e00ec59969ae9158e5b8f8b59f21e01e4b
nicolargo <nicolargo@nicolargo-fujitsu.(none)>
2011-12-05 21:07:50 +0100

    Google change agin the URL... i found it :)

--- chromium-on-mac.sh
+++ chromium-on-mac.sh
@@ -4,17 +4,18 @@
 # 
 # Nicolargo
 # GPL v3
-SCRIPT_VERSION="1.2"
+SCRIPT_VERSION="1.3"
 
-#CHROMIUM_URL="http://build.chromium.org/f/chromium/snapshots/Mac"
-CHROMIUM_URL="http://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac"
+CHROMIUM_URL="http://commondatastorage.googleapis.com/chromium-browser-continuous/index.html?path=Mac"
+CHROMIUM_URL2="http://commondatastorage.googleapis.com/chromium-browser-continuous/Mac"
 CHROMIUM_INSTALL_PATH="/Applications/"
 CHROMIUM_CURRENT_VERSION_FILE="$CHROMIUM_INSTALL_PATH/Chromium.app/Contents/Info.plist"
 CHROMIUM_CURRENT_VERSION=`defaults read /Applications/Chromium.app/Contents/Info SVNRevision`
 
 echo "Chromium version installed: $CHROMIUM_CURRENT_VERSION"
-echo "Checking for update"
-CHROMIUM_LATEST_VERSION=$(curl -s -S $CHROMIUM_URL/LAST_CHANGE)
+echo "Checking for update: $CHROMIUM_URL2/LAST_CHANGE"
+CHROMIUM_LATEST_VERSION=$(curl -s -S $CHROMIUM_URL2/LAST_CHANGE)
+echo "Latest Chromium version   :  $CHROMIUM_LATEST_VERSION"
 
 if [ "$CHROMIUM_CURRENT_VERSION" == "" ] || [ $CHROMIUM_CURRENT_VERSION -lt $CHROMIUM_LATEST_VERSION ]; then
 	echo "Latest version of Chromium will be installed"
@@ -24,10 +25,9 @@ if [ "$CHROMIUM_CURRENT_VERSION" == "" ] || [ $CHROMIUM_CURRENT_VERSION -lt $CHR
 		exit 1
 	fi
 	
-	echo "Latest Chromium version:  $CHROMIUM_LATEST_VERSION"
 	echo "Downloading Chromium version $CHROMIUM_LATEST_VERSION"
-	echo "URL = $CHROMIUM_URL/$CHROMIUM_LATEST_VERSION/chrome-mac.zip"
-	curl -L $CHROMIUM_URL/$CHROMIUM_LATEST_VERSION/chrome-mac.zip > /tmp/chrome-mac.zip
+	echo "URL = $CHROMIUM_URL2/$CHROMIUM_LATEST_VERSION/chrome-mac.zip"
+	curl -L $CHROMIUM_URL2/$CHROMIUM_LATEST_VERSION/chrome-mac.zip > /tmp/chrome-mac.zip
 	cd /tmp
 	unzip chrome-mac.zip
 	rm -rf $CHROMIUM_INSTALL_PATH/Chromium.app
